45c5b50022354f46320c8e7c4e8dec6ea4b1d0d7
rachel-cunningham <123897610+rachel-cunningham@users.noreply.github.com>
2024-02-21 17:19:37 -0500

    Update signup.router.js

--- back-end/src/signup/signup.router.js
+++ back-end/src/signup/signup.router.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const controller = require("./signup.controller"); 
 
-router.route("/additional_info/schedule_reminder").put(controller.add);
 router.route("/additional_info").put(controller.update);
 router.route("/").post(controller.create);
+router.route("/:user_id").get(controller.getInfoAndTime);
+
 
 module.exports = router;
